5c71f95a31bc2a703d39f11098ce2c6a7f823620
Catalina Iacob <iacobcatalinaelena@gmail.com>
2023-07-19 15:32:37 +0300

    Exercises- Java week 2

--- src/main/java/teme/java_course2_flow/Ex0a_WarmupFlow.java
+++ src/main/java/teme/java_course2_flow/Ex0a_WarmupFlow.java
@@ -77,32 +77,32 @@ class Ex0a_WarmupFlow {
 
     public static void main(String[] args) {
 
-//        System.out.println("\nwhichIsBigger():");
-//        whichIsBigger(3, 1);
-//        whichIsBigger(1, 3);
-//        whichIsBigger(3, 3);
-//
-//        System.out.println("\nmax(1,2,3)= " + max(1, 2, 3));
-//        System.out.println("max(3,7,1)= " + max(3, 7, 1));
-//        System.out.println("max(1,3,2)= " + max(1, 3, 2));
-//        System.out.println("max(3,3,1)= " + max(3, 3, 1));
-//        System.out.println("max(3,11,1)= " + max(3, 11, 1));
-//        System.out.println("max(1,3,1)= " + max(1, 3, 1));
+        System.out.println("\nwhichIsBigger():");
+        whichIsBigger(3, 1);
+        whichIsBigger(1, 3);
+        whichIsBigger(3, 3);
 
-//        System.out.println("\nxTimes('a',4)= '" + xTimes("a", 4) + "'");
-//        System.out.println("xTimes('Abc',2)= '" + xTimes("Abc", 2) + "'");
-//
-//        System.out.println("\nsumSquares(1)= " + sumSquares(1));
-//        System.out.println("sumSquares(4)= " + sumSquares(4));
+        System.out.println("\nmax(1,2,3)= " + max(1, 2, 3));
+        System.out.println("max(3,7,1)= " + max(3, 7, 1));
+        System.out.println("max(1,3,2)= " + max(1, 3, 2));
+        System.out.println("max(3,3,1)= " + max(3, 3, 1));
+        System.out.println("max(3,11,1)= " + max(3, 11, 1));
+        System.out.println("max(1,3,1)= " + max(1, 3, 1));
+
+        System.out.println("\nxTimes('a',4)= '" + xTimes("a", 4) + "'");
+        System.out.println("xTimes('Abc',2)= '" + xTimes("Abc", 2) + "'");
+
+        System.out.println("\nsumSquares(1)= " + sumSquares(1));
+        System.out.println("sumSquares(4)= " + sumSquares(4));
 
         System.out.println("\ndayOfWeek('Monday') = " + dayOfWeek("Monday"));
         System.out.println("dayOfWeek('DUMINICA') = " + dayOfWeek("DUMINICA"));
         System.out.println("dayOfWeek('ViNeRi') = " + dayOfWeek("ViNeRi"));
         System.out.println("dayOfWeek('invalid') = " + dayOfWeek("invalid"));
-//
-//        System.out.println("\nfactorial(1) = " + factorial(1));
-//        System.out.println("factorial(5) = " + factorial(5));
-//        System.out.println("factorial(13) = " + factorial(13));
-//    }
 
-    }}
+        System.out.println("\nfactorial(1) = " + factorial(1));
+        System.out.println("factorial(5) = " + factorial(5));
+        System.out.println("factorial(13) = " + factorial(13));
+    }
+
+    }
